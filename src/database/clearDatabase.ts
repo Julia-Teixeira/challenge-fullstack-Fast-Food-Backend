9636f7c92b69103e2b4687dfc2df45df6245819b
Julia Pereira Teixeira <juliapt95@gmail.com>
2024-03-19 23:17:01 -0300

    feat: add function that reset the database

---
+++ src/database/clearDatabase.ts
@@ -0,0 +1,37 @@
+import prisma from "./prisma";
+import { exec } from "child_process";
+
+async function aplicarMigracoes() {
+  return new Promise<void>((resolve, reject) => {
+    exec("npx prisma migrate dev").on("close", (codigoSaida) => {
+      if (codigoSaida === 0) {
+        console.log("Migrações aplicadas com sucesso.");
+        resolve();
+      } else {
+        const mensagemErro = `Erro ao aplicar migrações. Código de saída: ${codigoSaida}`;
+        console.error(mensagemErro);
+        reject(new Error(mensagemErro));
+      }
+    });
+  });
+}
+
+async function resetarBancoDeDados() {
+  try {
+    await prisma.product.deleteMany();
+    await prisma.category.deleteMany();
+    await prisma.additional.deleteMany();
+
+    // await prisma.$queryRaw`DELETE FROM categories RESTART WITH 1`;
+    // await prisma.$queryRaw`DELETE FROM categories;`;
+
+    await prisma.$executeRaw`TRUNCATE TABLE categories RESTART IDENTITY CASCADE;`;
+    await prisma.$executeRaw`TRUNCATE TABLE additional RESTART IDENTITY CASCADE;`;
+
+    await aplicarMigracoes();
+  } catch (error) {
+    console.error("Erro ao resetar o banco de dados:", error);
+  }
+}
+
+export default resetarBancoDeDados;
