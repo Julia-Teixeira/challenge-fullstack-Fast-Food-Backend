c97d157990732bf8cd42563fef05783fecb5436d
Julia Pereira Teixeira <juliapt95@gmail.com>
2024-03-24 22:49:05 -0300

    feat: deploy settings

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ import productOrderRouter from "./routes/productOrder.router";
 import orderRouter from "./routes/order.router";
 import HandleErrors from "./middleware/handleError.middleware";
 import swaggerUi from "swagger-ui-express";
-import fs from "fs";
+import * as fs from "fs";
 import YAML from "yaml";
 import categoriesRouter from "./routes/categories.router";
 import additionalRouter from "./routes/additional.router";
@@ -20,9 +20,8 @@ app.use(json());
 app.use(cors());
 app.use(helmet());
 
-const file = fs.readFileSync("./swagger.yaml", "utf8");
-const swaggerDocument = YAML.parse(file);
-app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const swaggerDocument = YAML.parse(fs.readFileSync("./swagger.yaml", "utf8"));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/products", productRouter);
 app.use("/additionals", additionalRouter);
